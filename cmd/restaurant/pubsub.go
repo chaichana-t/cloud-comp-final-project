42af02d601119269d6061d2761990787b5093088
Chi <54677538+originalchi@users.noreply.github.com>
2021-04-12 16:13:41 +0700

    check number of customer before check in and check out

--- cmd/restaurant/pubsub.go
+++ cmd/restaurant/pubsub.go
@@ -2,15 +2,40 @@ package restaurant
 
 import (
 	syncService "cloud-final-project/cmd/sync-service"
+	"fmt"
+	"strconv"
+
 	"github.com/gorilla/websocket"
 )
 
 func CheckIn(restaurantID string) {
+	numberOfCustomer, err := strconv.Atoi(syncService.Get(restaurantID))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if numberOfCustomer >= *&GetInfo(restaurantID).MaxCustomer {
+		fmt.Println("Full capacity")
+		return
+	}
+
 	value := syncService.Increase(restaurantID)
 	syncService.Publish(redisPayloadChannel, constructPayload(restaurantID, value))
 }
 
 func CheckOut(restaurantID string) {
+	numberOfCustomer, err := strconv.Atoi(syncService.Get(restaurantID))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if numberOfCustomer <= 0 {
+		fmt.Println("Conflict")
+		return
+	}
+
 	value := syncService.Decrease(restaurantID)
 	syncService.Publish(redisPayloadChannel, constructPayload(restaurantID, value))
 }
